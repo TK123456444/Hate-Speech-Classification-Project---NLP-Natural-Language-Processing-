26d0ab3f1371e6a1f2256b9493102dfd3df27ea2
Tushar Kumawat <tukumawat73@gmail.com>
2026-08-05 00:58:41 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path):
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
 
 setup(
     name="hate-speech-classification",
@@ -6,5 +19,5 @@ setup(
     author="Tushar",
     author_email="tukumawat73@gmail.com",
     packages=find_packages(),
-    install_requires="requirements.txt",
+    install_requires=get_requirements("requirements.txt"),
 )
